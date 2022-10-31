6248c0b379f90d60ee65b070ce780e4abe170496
NIKO <39039032+niko-dellic@users.noreply.github.com>
2022-10-31 15:02:04 -0400

    Update map.js

--- map.js
+++ map.js
@@ -35,7 +35,7 @@ fetch(
       mapboxApiAccessToken:
         "pk.eyJ1Ijoibmlrby1kZWxsaWMiLCJhIjoiY2w5c3p5bGx1MDh2eTNvcnVhdG0wYWxkMCJ9.4uQZqVYvQ51iZ64yG8oong",
       // Set your Mapbox style here
-      // mapStyle: "mapbox://styles/niko-dellic/cl9t226as000x14pr1hgle9az",
+      mapStyle: "mapbox://styles/niko-dellic/cl9t226as000x14pr1hgle9az",
       initialViewState: {
         latitude: 39.9526,
         longitude: -75.1652,
